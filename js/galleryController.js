82eee53bd0c83938179c59c368e3f1cc51728305
aviya-ha <aviyaharel6@gmail.com>
2024-03-03 23:29:16 +0200

    Add “search by keywords” to Image-Gallery Page

--- js/galleryController.js
+++ js/galleryController.js
@@ -1,10 +1,10 @@
 'use strict'
 
-var gImgs = [
+const gImgs = [
     { id: 1, url: 'img/1.jpg', keywords: ['men', 'angry'] },
-    { id: 2, url: 'img/2.jpg', keywords: ['dog', 'cute'] },
+    { id: 2, url: 'img/2.jpg', keywords: ['dog', 'funny'] },
     { id: 3, url: 'img/3.jpg', keywords: ['dog', 'baby',] },
-    { id: 4, url: 'img/4.jpg', keywords: ['cat', 'cute'] },
+    { id: 4, url: 'img/4.jpg', keywords: ['cat', 'funny'] },
     { id: 5, url: 'img/5.jpg', keywords: ['baby', 'angry'] },
     { id: 6, url: 'img/6.jpg', keywords: ['men', 'funny'] },
     { id: 7, url: 'img/7.jpg', keywords: ['baby', 'funny'] },
@@ -20,11 +20,35 @@ var gImgs = [
     { id: 17, url: 'img/17.jpg', keywords: ['men', 'angry'] },
     { id: 18, url: 'img/18.jpg', keywords: ['toy', 'funny'] },
 ]
+const gAllKeywords = [
+    {
+        id: 'men',
+        count: 5
+    },
+    {
+        id: 'angry',
+        count: 5
+    },
+    {
+        id: 'dog',
+        count: 5
+    },
+    {
+        id: 'baby',
+        count: 10
+    },
+    {
+        id: 'funny',
+        count: 5
+    }
+]
+
 
 
 
 function onInit() {
     renderGallery()
+    renderFilters()
 }
 
 function renderGallery() {
@@ -37,6 +61,35 @@ function renderGallery() {
     elImageContainer.innerHTML = strHtmls.join('')
 }
 
+function renderFilters() {
+    const elFilterPop = document.querySelector('.filter-pop')
+
+    var strHtmls = gAllKeywords.map(word => `
+    <span class="key-word ${word.id}" style=" font-size: ${word.count}px;" onclick="onKeyWordClick(this)">${word.id} </span> 
+    `)
+
+    elFilterPop.innerHTML = strHtmls.join('')
+}
+
+function onKeyWordClick(elSpan) {
+    var word = gAllKeywords.find(word => word.id === elSpan.innerText)
+    word.count += 2
+    const elImageContainer = document.querySelector('.main-image-content')
+    var filteredImg = filterImgs(elSpan.innerText)
+  
+    
+    
+    
+    console.log('filteredImg:', filteredImg)
+    var strHtmls = filteredImg.map(image =>
+        `
+        <img id="${image.id}" src="${image.url}" alt="${image.keywords}" title="${image.keywords}" onclick="onImgSelect(this)"> 
+        `)
+        
+        elImageContainer.innerHTML = strHtmls.join('')
+        renderFilters()
+}
+
 function onImgSelect({ id }) {
     setImg(id)
     const elImageContainer = document.querySelector('.main-image-container')
@@ -44,8 +97,8 @@ function onImgSelect({ id }) {
     onInitEdit()
 }
 
-function onImgSelectRandom(){
-    const randomId = getRandomInt(1,18)
+function onImgSelectRandom() {
+    const randomId = getRandomInt(1, 18)
     setImg(randomId)
     const elImageContainer = document.querySelector('.main-image-container')
     elImageContainer.style.display = 'none'
@@ -53,7 +106,7 @@ function onImgSelectRandom(){
 }
 
 
-function onGallery(){
+function onGallery() {
     const elImageContainer = document.querySelector('.main-image-container')
     elImageContainer.style.display = 'block'
     const elMemeContainer = document.querySelector('.main-meme-content')
@@ -63,16 +116,21 @@ function onGallery(){
 
 }
 
-function onInputFilter(elValue){
-    console.log('elValue:', elValue)
+function onInputFilter(elValue) {
+
+    var word = gAllKeywords.find(word => word.id === elValue)
+
+    word.count += 2
+    renderFilters()
+
     const elImageContainer = document.querySelector('.main-image-content')
 
- const filteredImg = gImgs.filter(img => 
-    img.keywords[0].includes(elValue) || img.keywords[1].includes(elValue))
+    const filteredImg = gImgs.filter(img =>
+        img.keywords[0].includes(elValue) || img.keywords[1].includes(elValue))
 
 
- console.log('filteredImg:', filteredImg)
-    var strHtmls = filteredImg.map(image => 
+    console.log('filteredImg:', filteredImg)
+    var strHtmls = filteredImg.map(image =>
         `
 <img id="${image.id}" src="${image.url}" alt="${image.keywords}" title="${image.keywords}" onclick="onImgSelect(this)"> 
     `)
@@ -80,7 +138,11 @@ function onInputFilter(elValue){
     elImageContainer.innerHTML = strHtmls.join('')
 }
 
-function onClearBtn(){
-     document.getElementById('filter-input').value=""
-     renderGallery()
+function onClearBtn() {
+    document.getElementById('filter-input').value = ""
+    renderGallery()
+}
+
+function filterImgs(text){
+    return gImgs.filter(img => img.keywords[0].includes(text) || img.keywords[1].includes(text))
 }
